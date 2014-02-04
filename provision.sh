aa3dc1fee5a2c77cc9f38c1640dfc5ab984c5457
Antonio Spinelli <antonio.spinelli@duosystem.com.br>
2014-02-04 13:15:26 -0200

    installing jenkins plugins and curl

--- provision.sh
+++ provision.sh
@@ -11,16 +11,18 @@ sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list
 apt-get update
 
 apt-get -y install vim
+apt-get -y install curl
 apt-get -y install git-core
 apt-get -y install jenkins
 apt-get -y install php5
 apt-get -y install php5-cli
 apt-get -y install php5-cgi
-apt-get -y install php5-soap
+apt-get -y install php5-intl
 apt-get -y install php5-mcrypt
 apt-get -y install php5-mbstring
-apt-get -y install php5-intl
+apt-get -y install php5-soap
 apt-get -y install php5-xdebug
+apt-get -y install php5-xsl
 apt-get -y install php-apc
 apt-get -y install php-pear
 
@@ -30,6 +32,10 @@ pear channel-discover pear.phing.info
 pear install phpqatools/phpqatools
 pear install -a phing/phing
 
+curl -s http://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer.phar && alias composer='/usr/local/bin/composer.phar'
+
+wget http://phpdox.de/releases/phpdox.phar && chmod +x phpdox.phar && mv phpdox.phar /usr/local/bin/phpdox && phpdox --version
+
 cp /vagrant/files/php.ini /etc/php5/cli/php.ini && echo "installed /etc/php5/cli/php.ini"
 
-/etc/init.d/jenkins start
+/etc/init.d/jenkins start && wget http://localhost:8080/jnlpJars/jenkins-cli.jar && java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin git phing checkstyle cloverphp dry htmlpublisher jdepend plot pmd violations xunit && java -jar jenkins-cli.jar -s http://localhost:8080 safe-restart
